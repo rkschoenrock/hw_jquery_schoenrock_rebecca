b5e9995f4113ce88e17e68d5517f96beef22438b
Rebecca Schoenrock <rkschoenrock@gmail.com>
2016-07-20 22:02:00 -0400

    Change colors of styled paragraphs

--- js/main.js
+++ js/main.js
@@ -1,4 +1,4 @@
 $(document).ready(function () {
     var paragraphs = $('p');
-    paragraphs.css({ 'background-color': '#262626', 'color': '#ffffff', 'letter-spacing': '2px' });
+    paragraphs.css({ 'background-color': '#d3d3d3', 'color': 'olive', 'letter-spacing': '2px' });
 });
